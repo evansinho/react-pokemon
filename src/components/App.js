6dc765f94ba3300376ae4069a534b744ec444b0f
evansinho <igiri.evanson@gmail.com>
2020-08-17 21:57:23 +0100

    set up router and create the show pokemon component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PokemonList from '../container/PokemonList';
+import ShowPokemon from '../container/ShowPokemon';
+
 import '../styles/App.css';
 
 function App() {
   return (
-    <div className="container">
-      <div className="nav">
-        <h1>POKEDEX</h1>
-        <input type="text" placeholder="SEARCH" />
+    <Router>
+      <div className="container">
+        <div className="nav">
+          <h1>POKEDEX</h1>
+          <input type="text" placeholder="SEARCH" />
+        </div>
+        <Switch>
+          <Route exact path="/" component={PokemonList} />
+          <Route exact path="/pokemon/:id" component={ShowPokemon} />
+        </Switch>
       </div>
-      <PokemonList />
-    </div>
+    </Router>
   );
 }
 
---
+++ src/container/ShowPokemon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ShowPokemon = () => (
+  <div>
+    <h1>Show</h1>
+  </div>
+);
+
+export default ShowPokemon;
